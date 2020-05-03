0926ec7979fdc7609ecc4ac2fd3a7c1febd9b3c3
aerlich <axel.erlich@yahoo.com.ar>
2020-05-03 16:18:34 -0300

    Agrego script de inicio y el recepcionista mira el canasto

--- Recepcionista/recepcionista.cpp
+++ Recepcionista/recepcionista.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <string.h>
 #include <stdlib.h>
+#include "../Utility/utility.h"
+#include "../Utility/memoriaCompartida.h"
 
 #define PIZZA "pizza"
 #define PAN "pan"
 #define FIN "fin"
 
+
 using namespace std;
 
-//// EL RECEPCIONISTA LO QUE HACE ES SIMPLEMENTE RECIBIR UN PEDIDO Y DELEGARLO A ALGUN MAESTRO SEGUN CORRESPONDA
-//// RECIBE POR STDIN EL PEDIDO Y LO DEVUELVE POR STDOUT
+//// EL RECEPCIONISTA LO QUE HACE ES SIMPLEMENTE RECIBIR UN PEDIDO
+//// RECIBE POR STDIN EL PEDIDO Y LO DEVUELVE LA RESPUESTA POR STDOUT
+//// EN CASO DE QUE EL PEDIDO SEA UN PAN, SE VA A FIJAR AL CANASTO SI HAY PAN, EN CASO DE NO HABER DEVUELVE QUE NO HAY
+//// EN CASO DE QUE EL PEDIDO SEA UNA PIZZA, DELEGA EL PEDIDO AL MAESTRO PIZZERO
 
 void hacerPedido(string pedido);
 
 int main () {
     
-    string pedido = "";
-
-    cout << "Hola soy un recepcionista" << endl;
-
+    
+    string pedido;
 
     cout << "Esperando pedido..." << endl;
     cin >> pedido;
@@ -34,18 +37,30 @@ int main () {
 }
 
 void hacerPedido(string pedido) {
+    string archivo = "/bin/ls";
+    MemoriaCompartida<int> canasta (archivo, 'A');
 
-
-    if (pedido.compare(PAN) == 0){
-        cout << "Preparando pan" << endl;
+    if (pedido.compare(PAN) == 0) {
         // En el caso del pan, tengo que ir a la canasta y ver si hay algun pan para
+        int panes = canasta.leer();
+        
+        if (panes > 0) {
+            cout << obtenerFechaYHora() << " - Recepcionista: Aqui tienes tu pan..." << endl;
+            panes--;
+            canasta.escribir(panes);
+            cout << "Cantidad de panes que quedan: " << canasta.leer() << endl;
+        } else {
+            cout << "Disculpe, no hay mas pan"<< endl;
+        }
+        
+        
 
 
-    } else if (pedido.compare(PIZZA) == 0){
+    } else if (pedido.compare(PIZZA) == 0) {
         cout << "Preparando pizza" << endl;
         // En el caso de la pizza tengo que hacer el pedido a un maestro pizzero y esperar que la cocine
 
-    } else{
+    } else {
         cout << "El pedido " << pedido << " no forma parte del menu." << endl;
     }
 
---
+++ run.sh
@@ -0,0 +1,8 @@
+cd MaestroEspecialista/
+./maestroEspecialista &
+cd ..
+cd MaestroPanadero/
+./maestroPanadero &
+cd ..
+cd Recepcionista/
+./recepcionista && fg
